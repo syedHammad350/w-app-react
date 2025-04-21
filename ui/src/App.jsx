0cd2d2b8a4f0b790b3afdcd53642e8962173648c
syedHammad350 <ahmed22ali456@gmail.com>
2025-04-21 10:00:52 -0700

    adding routes

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -3,12 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Weather } from './Component/weather'
+import { Route, Routes } from 'react-router'
 
 function App() {
   
 
   return (
-    <Weather></Weather>
+    <Routes>
+      <Route path='/'element={<Weather/>}/>
+    </Routes>
   )
 }
 
